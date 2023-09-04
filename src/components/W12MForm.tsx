1949f5b26ffb77cb2766588c7db5cb638e097060
M Zia <murtaza_zia@hotmail.co.uk>
2023-09-04 18:51:06 +0100

    added the input for the 2+2 question in activity 3

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -3,14 +3,14 @@ import W12MHeader from './W12MHeader';
 import { SpeciesName } from './speciesName';
 import { PlanetName } from './PlanetName';
 import { NumberOfBeings } from './NumberOfBeings';
-
+import { TwoPlusTwo } from './twoPlusTwo';
 
 const W12MForm = () => {
 
 	const [speciesName, setSpeciesName] = useState('humans');
-const [planetName, setPlanetName] = useState('Earth');
-const [numberOfBeings, setNumberOfBeings] = useState('0')
-
+	const [planetName, setPlanetName] = useState('Earth');
+	const [numberOfBeings, setNumberOfBeings] = useState('0')
+	const [twoPlusTwo, setTwoPlusTwo] = useState('')
 
 	return (
 		<section className='w12MForm'>
@@ -18,9 +18,9 @@ const [numberOfBeings, setNumberOfBeings] = useState('0')
 
 			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(value) => setSpeciesName(value)} />
 			<PlanetName planetName={planetName} onChangePlanetName={(value) => setPlanetName(value)} />
-
-			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(value) => setSpeciesName(value)} />
 			<NumberOfBeings numberOfBeings={numberOfBeings} onChangeBeingsNumber={(value) => setNumberOfBeings(value)} />
+			<TwoPlusTwo twoPlusTwo={twoPlusTwo} onChangeTwoPlusTwo={(value) => setTwoPlusTwo(value)} />
+
 		</section>
 	);
 };
---
+++ src/components/twoPlusTwo.tsx
@@ -0,0 +1,18 @@
+interface TwoPlusTwo {
+    twoPlusTwo: string;
+    onChangeTwoPlusTwo: (value: string) => void;
+}
+
+export const TwoPlusTwo: React.FC<TwoPlusTwo> = ({ twoPlusTwo, onChangeTwoPlusTwo }) => (
+    <>
+        <div>
+            <label htmlFor='twoPlusTwo'>What is 2+2 ?</label>
+            <select id='twoPlusTwo'
+                value={twoPlusTwo}
+                onChange={(e) => onChangeTwoPlusTwo(e.target.value)}>
+                <option value="4">4</option>
+                <option value="Not 4">Not 4</option>
+            </select>
+        </div>
+    </>
+);
